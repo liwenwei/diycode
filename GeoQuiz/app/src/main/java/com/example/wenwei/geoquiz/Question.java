50e41ef3b2b63ed4d98889d9203c66ed7fc95039
liwenwei <liwenwei06@126.com>
2017-10-10 08:31:03 +0800

    GeoQuiz APP： 使用MVC模式，创建View

---
+++ GeoQuiz/app/src/main/java/com/example/wenwei/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.example.wenwei.geoquiz;
+
+/**
+ * Created by liwenwei on 2017/10/10.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
