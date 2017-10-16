f70c12eadf8b4e95e22818914ed8edbd3fcb1048
liwenwei <liwenwei06@126.com>
2017-10-16 21:15:38 +0800

    7. 创建新项目CriminalIntent

---
+++ CriminalIntent/app/src/main/java/com/example/wenwei/criminalintent/Crime.java
@@ -0,0 +1,49 @@
+package com.example.wenwei.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by v-wenwli on 10/16/2017.
+ */
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean ismSolved() {
+        return mSolved;
+    }
+
+    public void setmSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+}
