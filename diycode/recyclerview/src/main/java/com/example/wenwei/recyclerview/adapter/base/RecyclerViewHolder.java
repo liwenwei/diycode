fc822a8531a59daa3fec364650b05a133bd8591d
liwenwei <liwenwei06@126.com>
2017-11-20 18:16:52 +0800

    添加RecycleView工具包，完成Layout Manager和SingleTypeAdapter

---
+++ diycode/recyclerview/src/main/java/com/example/wenwei/recyclerview/adapter/base/RecyclerViewHolder.java
@@ -0,0 +1,53 @@
+package com.example.wenwei.recyclerview.adapter.base;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * 布局持有者
+ */
+public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+    private View mRootView;
+
+    public RecyclerViewHolder(View itemView) {
+        super(itemView);
+        mRootView = itemView;
+    }
+
+    public View getRootView() {
+        return mRootView;
+    }
+
+    private final SparseArray<View> mViews = new SparseArray<>();
+
+    private <T extends View> T bindView(int id) {
+        T view = (T) mViews.get(id);
+        if (view == null) {
+            view = (T) mRootView.findViewById(id);
+            mViews.put(id, view);
+        }
+        return view;
+    }
+
+    public <T extends View> T get(int id) {
+        return (T) bindView(id);
+    }
+
+    public void setOnClickListener(View.OnClickListener l, int... ids) {
+        if (ids == null) {
+            return;
+        }
+        for (int id : ids) {
+            get(id).setOnClickListener(l);
+        }
+    }
+
+    public void setText(int id, String text) {
+        TextView textView = get(id);
+        textView.setText(text);
+    }
+}
---
+++ diycode/recyclerview/src/main/java/com/example/wenwei/recyclerview/adapter/singletype/SingleTypeAdapter.java
@@ -0,0 +1,73 @@
+package com.example.wenwei.recyclerview.adapter.singletype;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wenwei.recyclerview.adapter.base.RecyclerViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class SingleTypeAdapter<T> extends RecyclerView.Adapter<RecyclerViewHolder> {
+
+    protected Context mContext;
+    private LayoutInflater mInflater;
+    private List<T> mDatas = new ArrayList<>();
+    private int mLayoutId;
+
+    public SingleTypeAdapter(@NonNull Context context, @LayoutRes int layoutId) {
+        mInflater = LayoutInflater.from(context);
+        mContext = context;
+        mLayoutId = layoutId;
+    }
+
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View rootView = null;
+        try {
+            rootView = mInflater.inflate(mLayoutId, parent, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new RecyclerViewHolder(rootView);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+        convert(position, holder, mDatas.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+
+    /**
+     * 在此处处理数据
+     *
+     * @param position 位置
+     * @param holder   view holder
+     * @param bean     数据
+     */
+    public abstract void convert(int position, RecyclerViewHolder holder, T bean);
+
+    public void addDatas(List<T> datas) {
+        this.mDatas.addAll(datas);
+        notifyDataSetChanged();
+    }
+
+    public List<T> getDatas() {
+        return mDatas;
+    }
+
+    public void clearDatas() {
+        this.mDatas.clear();
+        notifyDataSetChanged();
+    }
+
+}
