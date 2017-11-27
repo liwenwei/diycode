c2ec27b1c1eae335486c34d03408abd7c5885b56
liwenwei <liwenwei06@126.com>
2017-11-27 19:26:04 +0800

    Diycode: 完成app base activity和ViewHolder

---
+++ diycode/diycode-app/src/main/java/com/example/wenwei/diycode/app/BaseActivity.java
@@ -0,0 +1,30 @@
+package com.example.wenwei.diycode.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.wenwei.diycode_sdk.api.Diycode;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    protected Diycode mDiycode;
+    protected RecyclerView.ViewHolder mViewHolder;
+    private Toast mToast;
+
+    /**
+     * 初始化数据，调用位置在 initViews 之前
+     */
+    protected void initDatas() {
+    }
+
+    /**
+     * 初始化 View， 调用位置在 initDatas 之后
+     */
+    protected abstract void initViews(ViewHolder holder, View root);
+
+    private void initActionBar(ViewHolder holder){
+
+    }
+}
---
+++ diycode/diycode-app/src/main/java/com/example/wenwei/diycode/app/ViewHolder.java
@@ -0,0 +1,5 @@
+package com.example.wenwei.diycode.app;
+
+public class ViewHolder {
+
+}
