9a7e2acb4914590c3d94ecf581b1dcae2fc812eb
liwenwei <liwenwei06@126.com>
2017-11-28 15:47:05 +0800

    Diycode: Implements Glide for image loading and caching

---
+++ diycode/diycode-app/src/main/java/com/example/wenwei/diycode/glide/BaseGlideModule.java
@@ -0,0 +1,27 @@
+package com.example.wenwei.diycode.glide;
+
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.load.engine.cache.ExternalCacheDiskCacheFactory;
+import com.bumptech.glide.load.engine.cache.LruResourceCache;
+import com.bumptech.glide.module.GlideModule;
+
+public class BaseGlideModule implements GlideModule {
+
+    int DEFAULT_DISK_CACHE_SIZE = 250 * 1024 * 1024;
+    int M = 1024 * 1024;
+
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        builder.setMemoryCache(new LruResourceCache(10 * M));
+        builder.setDiskCache(new ExternalCacheDiskCacheFactory(context, "glide-cache",
+                DEFAULT_DISK_CACHE_SIZE));
+    }
+
+    @Override
+    public void registerComponents(Context context, Glide glide) {
+
+    }
+}
---
+++ diycode/diycode-app/src/main/java/com/example/wenwei/diycode/glide/UrlDrawable.java
@@ -0,0 +1,76 @@
+package com.example.wenwei.diycode.glide;
+
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+
+/**
+ * 为 {@Link GlideImageGetter} 服务
+ */
+public class UrlDrawable extends Drawable implements Drawable.Callback {
+
+    private Drawable mDrawable;
+
+    @Override
+    public void draw(@NonNull Canvas canvas) {
+        if (mDrawable != null) {
+            mDrawable.draw(canvas);
+        }
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        if (mDrawable != null) {
+            mDrawable.setAlpha(alpha);
+        }
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+        if (mDrawable != null) {
+            mDrawable.setColorFilter(colorFilter);
+        }
+    }
+
+    @Override
+    public int getOpacity() {
+        if (mDrawable != null) {
+            return mDrawable.getOpacity();
+        }
+        return 0;
+    }
+
+    public void setDrawable(GlideDrawable drawable) {
+        if (this.mDrawable != null) {
+            this.mDrawable.setCallback(null);
+        }
+        drawable.setCallback(this);
+        this.mDrawable = drawable;
+    }
+
+    @Override
+    public void invalidateDrawable(@NonNull Drawable who) {
+        if (getCallback() != null) {
+            getCallback().invalidateDrawable(who);
+        }
+    }
+
+    @Override
+    public void scheduleDrawable(@NonNull Drawable who, @NonNull Runnable what, long when) {
+        if (getCallback() != null) {
+            getCallback().scheduleDrawable(who, what, when);
+        }
+    }
+
+    @Override
+    public void unscheduleDrawable(@NonNull Drawable who, @NonNull Runnable what) {
+        if (getCallback() != null) {
+            getCallback().unscheduleDrawable(who, what);
+        }
+    }
+}
