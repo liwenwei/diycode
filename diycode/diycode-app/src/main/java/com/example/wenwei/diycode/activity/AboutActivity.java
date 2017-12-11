870bdff4da0dffc2263b7b85247d5e8dc93907bc
liwenwei <liwenwei06@126.com>
2017-12-11 16:41:20 +0800

    Diycode: 添加 about activity以及相应layout

---
+++ diycode/diycode-app/src/main/java/com/example/wenwei/diycode/activity/AboutActivity.java
@@ -0,0 +1,38 @@
+package com.example.wenwei.diycode.activity;
+
+
+import android.view.View;
+
+import com.example.wenwei.diycode.R;
+import com.example.wenwei.diycode.base.app.BaseActivity;
+import com.example.wenwei.diycode.base.app.ViewHolder;
+import com.example.wenwei.diycode.utils.IntentUtil;
+
+public class AboutActivity extends BaseActivity implements View.OnClickListener {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_about;
+    }
+
+    @Override
+    protected void initViews(ViewHolder holder, View root) {
+        setTitle("关于");
+        holder.setOnClickListener(this, R.id.feed_back, R.id.github, R.id.contribute);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.feed_back:
+                IntentUtil.openUrl(this,"https://github.com/GcsSloop/diycode/issues/1");
+                break;
+            case R.id.github:
+                IntentUtil.openUrl(this,"https://github.com/GcsSloop");
+                break;
+            case R.id.contribute:
+                IntentUtil.openAlipay(this);
+                break;
+        }
+    }
+}
