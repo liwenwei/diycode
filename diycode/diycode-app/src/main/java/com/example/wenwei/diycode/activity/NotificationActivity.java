cdf5d13a8f9315acbff9007bb099a42a9f2c358e
liwenwei <liwenwei06@126.com>
2017-12-11 19:36:05 +0800

    Diycode: 1. 添加notification activity 以及相关layout 2. 添加Notification provider

---
+++ diycode/diycode-app/src/main/java/com/example/wenwei/diycode/activity/NotificationActivity.java
@@ -0,0 +1,31 @@
+package com.example.wenwei.diycode.activity;
+
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+
+import com.example.wenwei.diycode.R;
+import com.example.wenwei.diycode.base.app.BaseActivity;
+import com.example.wenwei.diycode.base.app.ViewHolder;
+import com.example.wenwei.diycode.fragment.NotificationsFragment;
+
+/**
+ * 查看通知
+ */
+public class NotificationActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.item_notification;
+    }
+
+    @Override
+    protected void initViews(ViewHolder holder, View root) {
+        setTitle("通知");
+        NotificationsFragment fragment = NotificationsFragment.newInstance();
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction()
+                .add(R.id.fragment, fragment)
+                .commit();
+    }
+}
---
+++ diycode/diycode-app/src/main/java/com/example/wenwei/diycode/fragment/NotificationsFragment.java
@@ -0,0 +1,91 @@
+package com.example.wenwei.diycode.fragment;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.wenwei.diycode.fragment.base.SimpleRefreshRecyclerFragment;
+import com.example.wenwei.diycode.fragment.provider.NotificationsProvider;
+import com.example.wenwei.diycode_sdk.api.notifications.bean.Notification;
+import com.example.wenwei.diycode_sdk.api.notifications.event.GetNotificationsListEvent;
+import com.example.wenwei.recyclerview.adapter.multitype.HeaderFooterAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 通知列表
+ */
+public class NotificationsFragment extends SimpleRefreshRecyclerFragment<Notification,
+        GetNotificationsListEvent> {
+    private static String TYPE_NodeChanged = "NodeChanged";             // 节点变更
+    private static String TYPE_TopicReply = "TopicReply";               // Topic 回复
+    private static String TYPE_NewsReply = "Hacknews";                  // News  回复
+    private static String TYPE_Mention = "Mention";                     // 有人提及
+
+    private static String MENTION_TYPE_TopicReply = "Reply";            // - Topic 回复中提及
+    private static String MENTION_TYPE_NewReply = "HacknewsReply";      // - News  回复中提及
+    private static String MENTION_TYPE_ProjectReply = "ProjectReply";   // - 项目   回复中提及
+
+    public static NotificationsFragment newInstance() {
+        Bundle args = new Bundle();
+        NotificationsFragment fragment = new NotificationsFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void initData(HeaderFooterAdapter adapter) {
+        loadMore();
+    }
+
+    @Override
+    protected void setAdapterRegister(Context context, RecyclerView recyclerView,
+                                      HeaderFooterAdapter adapter) {
+        adapter.register(Notification.class, new NotificationsProvider(getContext()));
+    }
+
+    @NonNull
+    @Override
+    protected String request(int offset, int limit) {
+        return mDiycode.getNotificationsList(offset, limit);
+    }
+
+    @Override
+    protected void onRefresh(GetNotificationsListEvent event, HeaderFooterAdapter adapter) {
+        List<Notification> data = clearData(event.getBean());
+        adapter.clearDatas();
+        adapter.addDatas(data);
+        toast("刷新成功");
+    }
+
+    @Override
+    protected void onLoadMore(GetNotificationsListEvent event, HeaderFooterAdapter adapter) {
+        List<Notification> data = clearData(event.getBean());
+        adapter.addDatas(data);
+    }
+
+    /**
+     * 清洗数据，主要清洗对象
+     * 1. HackNew 的回复 type = Hacknews
+     * 2. HackNew 的提及 type = Mention, mention_type = HacknewsReply
+     * 3. Project 的提及 type = Mention, mention_type = ProjectReply
+     * <p>
+     * 保留数据
+     * 1. Topic 的回复 type = TopicReply
+     * 2. Topic 的提及 type = Mention, mention_type = Reply
+     */
+    public List<Notification> clearData(List<Notification> datas) {
+        List<Notification> clearDatas = new ArrayList<>();
+        for (Notification data : datas) {
+            if (data.getType().equals(TYPE_TopicReply) ||
+                    (data.getType().equals(TYPE_Mention) && data.getMention_type().equals
+                            (MENTION_TYPE_TopicReply))) {
+                clearDatas.add(data);
+            }
+        }
+        return clearDatas;
+    }
+}
