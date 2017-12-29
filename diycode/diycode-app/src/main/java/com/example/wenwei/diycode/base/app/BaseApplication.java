de92217f21bb485a43f1be6e756c74fbf73376bd
liwenwei <liwenwei06@126.com>
2017-12-29 17:13:02 +0800

    Diycode: Report & Analysis Crash and ANR by Bugly

--- diycode/diycode-app/src/main/java/com/example/wenwei/diycode/base/app/BaseApplication.java
+++ diycode/diycode-app/src/main/java/com/example/wenwei/diycode/base/app/BaseApplication.java
@@ -1,11 +1,15 @@
 package com.example.wenwei.diycode.base.app;
 
 import android.app.Application;
+import android.content.Context;
 
+import com.example.wenwei.diycode.R;
 import com.example.wenwei.diycode.utils.Config;
 import com.example.wenwei.diycode.utils.CrashHandler;
+import com.example.wenwei.diycode.utils.ProcessUtil;
 import com.example.wenwei.diycode_sdk.api.Diycode;
 import com.squareup.leakcanary.LeakCanary;
+import com.tencent.bugly.crashreport.CrashReport;
 
 public class BaseApplication extends Application {
 
@@ -21,9 +25,29 @@ public class BaseApplication extends Application {
         LeakCanary.install(this);
 
         CrashHandler.getInstance().init(this);
+        initCrashReport();
 
         Diycode.init(this, client_id, client_secret);
 
         Config.init(this);
+    }
+
+    /**
+     * 如果App使用了多进程且各个进程都会初始化Bugly（例如在Application类onCreate()中初始化Bugly），
+     * 那么每个进程下的Bugly都会进行数据上报，造成不必要的资源浪费。
+     * <p>
+     * 因此，为了节省流量、内存等资源，建议初始化的时候对上报进程进行控制，只在主进程下上报数据：判断是否
+     * 是主进程（通过进程名是否为包名来判断），并在初始化Bugly时增加一个上报进程的策略配置。
+     */
+    private void initCrashReport() {
+        Context context = getApplicationContext();
+        String packageName = context.getPackageName();
+        String processName = ProcessUtil.getProcessName(android.os.Process.myPid());
+        // 设置是否为上报进程
+        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(context);
+        strategy.setUploadProcess(processName == null || processName.equals(packageName));
+        // 初始化Bugly
+        CrashReport.initCrashReport(context, strategy);
+
     }
 }
